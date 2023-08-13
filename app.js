0fcfd682e59d1294c27645972a9657d1ba0070ad
DanielDDsam <salaodxgotico91@gmail.com>
2023-08-13 17:19:28 -0400

    first commit

--- app.js
+++ app.js
@@ -1,41 +1,61 @@
-const { createBot, createProvider, createFlow, addKeyword } = require('@bot-whatsapp/bot')
+const { createBot, createProvider, createFlow, addKeyword } = require('@bot-whatsapp/bot');
+const QRPortalWeb = require('@bot-whatsapp/portal');
+const BaileysProvider = require('@bot-whatsapp/provider/baileys');
+const MockAdapter = require('@bot-whatsapp/database/mock');
+const fetch = require('node-fetch'); // Agregamos esta línea para importar 'node-fetch'
 
-const QRPortalWeb = require('@bot-whatsapp/portal')
-const BaileysProvider = require('@bot-whatsapp/provider/baileys')
-const MockAdapter = require('@bot-whatsapp/database/mock')
+// Configura tu clave de API de pronóstico del tiempo
+const WEATHER_API_KEY = '14bae0fad7a7b351359804e252851579';
+const LOCATION = 'valparaiso';
 
+async function getWeather() {
+  const url = `http://api.openweathermap.org/data/2.5/weather?q=${LOCATION}&appid=${WEATHER_API_KEY}&units=metric`;
 
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
 
+    const weatherDescription = data.weather[0].description;
+    const temperature = data.main.temp;
 
-
-
-const flowClima = addKeyword(['clima', 'temperatura']).addAnswer('🌡️ La temperatura actual es de 30°C')
-        
-const flowPrincipal = addKeyword(['hola', 'ole', 'alo'])
-    .addAnswer('🙌 Hola bienvenido a este *CalorcitoBot*')
-    .addAnswer(
-        [
-            'Me puedes escribir los siguiente comandos:',
-            '👉 *Clima* y te dire la temperatura actual',
-            '👉 *cumplido*  y te diré algo lindo',
-        ],
-        null,
-        null,
-        [flowClima]
-    )
-
-const main = async () => {
-    const adapterDB = new MockAdapter()
-    const adapterFlow = createFlow([flowPrincipal])
-    const adapterProvider = createProvider(BaileysProvider)
-
-    createBot({
-        flow: adapterFlow,
-        provider: adapterProvider,
-        database: adapterDB,
-    })
-
-    QRPortalWeb()
+    const weatherMessage = `El clima en ${LOCATION} es ${weatherDescription} con una temperatura de ${temperature}°C.`;
+    return weatherMessage;
+  } catch (error) {
+    console.error('Error al obtener el pronóstico del tiempo:', error);
+    return 'No se pudo obtener el pronóstico del tiempo.';
+  }
 }
 
-main()
+const flowClima = addKeyword(['clima', 'temperatura']).addAnswer(async () => {
+  const weatherMessage = await getWeather();
+  return weatherMessage;
+});
+
+const flowPrincipal = addKeyword(['hola', 'ole', 'alo'])
+  .addAnswer('🙌 Hola bienvenido a este *CalorcitoBot*')
+  .addAnswer(
+    [
+      'Me puedes escribir los siguientes comandos:',
+      '👉 *Clima* y te diré la temperatura actual',
+      '👉 *cumplido*  y te diré algo lindo',
+    ],
+    null,
+    null,
+    [flowClima]
+  );
+
+const main = async () => {
+  const adapterDB = new MockAdapter();
+  const adapterFlow = createFlow([flowPrincipal]);
+  const adapterProvider = createProvider(BaileysProvider);
+
+  createBot({
+    flow: adapterFlow,
+    provider: adapterProvider,
+    database: adapterDB,
+  });
+
+  QRPortalWeb();
+};
+
+main();
